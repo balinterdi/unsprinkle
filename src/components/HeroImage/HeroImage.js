ccd6455067efe691ae99332818b9be1c5554d14f
Balint Erdi <balint@balinterdi.com>
2022-10-28 12:13:17 +0200

    Exercise 2: Improve images

---
+++ src/components/HeroImage/HeroImage.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components/macro';
+
+const HeroImage = () => {
+  return (
+    <Picture>
+      <source
+        type="image/avif"
+        srcSet="
+          /images/hero-img.avif 1x,
+          /images/hero-img@2x.avif 2x,
+          /images/hero-img@3x.avif 3x
+        "
+      />
+      <source
+        type="image/jpeg"
+        srcSet="
+          /images/hero-img.jpg 1x,
+          /images/hero-img@2x.jpg 2x,
+          /images/hero-img@3x.jpg 3x
+        "
+      />
+      <Image
+        alt="Cat with cool blue eyes"
+        src="/images/hero-img.jpg" 
+      />
+    </Picture>
+  );
+};
+
+const Picture = styled.picture`
+  display: block;
+  width: 500px;
+  height: 500px;
+  max-height: 100%;
+`;
+
+const Image = styled.img`
+  width: 100%;
+`;
+
+export default HeroImage;
